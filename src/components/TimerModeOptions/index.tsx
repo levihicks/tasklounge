c298a6317bd1f8e9426da90f76a6336a146f663f
levi hicks <lv.hicks@gmail.com>
2021-08-25 15:02:46 -0500

    remove icons from timer page

--- src/components/TimerModeOptions/index.tsx
+++ src/components/TimerModeOptions/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { useAppDispatch, useAppSelector } from '../../hooks/typedReduxHooks';
 import { setPomodoroMode } from '../../store/timerSlice';
-import RegularTimerIcon from '../../assets/timer.png';
-import PomodoroTimerIcon from '../../assets/tomato.png';
 
 const TimerModeOption = styled.div<{active: boolean}>`
     font-size: ${props => props.theme.fontSizes.small};        
@@ -12,12 +10,11 @@ const TimerModeOption = styled.div<{active: boolean}>`
     display: flex;
     align-items: center;
     margin-right: 15px;
+    border-radius: 25px;
+    padding: 15px;
     opacity: ${props => props.active ? '1' : '0.5'};
-`;
-
-const TimerIcon = styled.img`
-    height: 32px;
-    margin-right: 5px;
+    ${props => props.active && `color: ${props.theme.colors.white};`}
+    ${props => props.active && `background: ${props.theme.colors.navy};`}
 `;
 
 const TimerModeOptions = () => {
@@ -27,11 +24,9 @@ const TimerModeOptions = () => {
     return (
         <>
             <TimerModeOption active={!pomodoroModeOn} onClick={() => dispatch(setPomodoroMode(false))}>
-                <TimerIcon src={RegularTimerIcon} alt='' />
                 Regular
             </TimerModeOption>
             <TimerModeOption active={pomodoroModeOn} onClick={() => dispatch(setPomodoroMode(true))}>
-                <TimerIcon src={PomodoroTimerIcon} alt='' />
                 Pomodoro
             </TimerModeOption>
         </>
